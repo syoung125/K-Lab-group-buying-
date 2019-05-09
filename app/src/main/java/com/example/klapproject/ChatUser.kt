b50c3504c1461cd01ed502c4e570f641b85d6c22
yeunjin1 <48876807+yeunjin1@users.noreply.github.com>
2019-05-09 19:22:30 +0900

    fragment3수정

---
+++ app/src/main/java/com/example/klapproject/ChatUser.kt
@@ -0,0 +1,6 @@
+package com.example.klapproject
+
+import android.widget.ImageView
+
+data class ChatUser(val id:String, val content:String, val userImg:Int, val itemImg:Int) {
+}
--- app/src/main/java/com/example/klapproject/Fragment3.kt
+++ app/src/main/java/com/example/klapproject/Fragment3.kt
@@ -5,10 +5,24 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.fragment_fragment3.*
 
 class Fragment3 : Fragment() {
 
+    lateinit var chatList:ArrayList<ChatUser>
+    lateinit var adapter:ChatAdapter
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_fragment3, container, false)
+        init()
+        return inflater.inflate(R.layout.fragment_fragment1, container, false)
+    }
+
+    fun init(){
+        chatList.add(ChatUser("seoyoung", "안녕하세요", resources.getIdentifier("presence_online", "drawable", activity?.packageName), resources.getIdentifier("sym_def_app_icon", "drawable", activity?.packageName)))
+        adapter = ChatAdapter(activity!!.applicationContext, R.layout.room_row, chatList)
+        listView.adapter = adapter
     }
 }
