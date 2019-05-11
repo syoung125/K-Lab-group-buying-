7356d68e6c55da8b407ae1415b73bf35dba994ef
syoung125 <laura120555@gmail.com>
2019-05-12 01:00:08 +0900

    firebase 연동 코드 추가

--- app/src/main/java/com/example/klapproject/WritingForm.kt
+++ app/src/main/java/com/example/klapproject/WritingForm.kt
@@ -1,12 +1,22 @@
 package com.example.klapproject
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import kotlinx.android.synthetic.main.activity_writing_form.*
 
 class WritingForm : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writing_form)
+        init()
+    }
+    fun init(){
+        regi_product.setOnClickListener {
+            var gohomeintent = Intent(this, MainActivity::class.java)
+            startActivity(gohomeintent)
+        }
     }
 }
