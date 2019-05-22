7264d06990295bd3f93f944604675373825c7b73
RessChild <wpqldmlrna@naver.com>
2019-05-22 22:42:34 +0900

    우선 버튼 intent

--- app/src/main/java/com/example/klapproject/Fragment1.kt
+++ app/src/main/java/com/example/klapproject/Fragment1.kt
@@ -5,11 +5,43 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.klapproject.R
+import kotlinx.android.synthetic.main.fragment_fragment1.view.*
 
 class Fragment1 : Fragment() {
 
+    interface makeIntent{
+        fun makeIntent(num:Int);
+    }
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_fragment1, container, false)
+        val view = inflater.inflate(R.layout.fragment_fragment1, container, false)
+
+        view.imgButton1.setOnClickListener {
+            if(activity is makeIntent)
+                (activity as makeIntent).makeIntent(1)
+        }
+        view.imgButton2.setOnClickListener {
+            if(activity is makeIntent)
+                (activity as makeIntent).makeIntent(2)
+        }
+        view.imgButton3.setOnClickListener {
+            if(activity is makeIntent)
+                (activity as makeIntent).makeIntent(3)
+        }
+        view.imgButton4.setOnClickListener {
+            if(activity is makeIntent)
+                (activity as makeIntent).makeIntent(4)
+        }
+        view.imgButton5.setOnClickListener {
+            if(activity is makeIntent)
+                (activity as makeIntent).makeIntent(5)
+        }
+        view.imgButton6.setOnClickListener {
+            if(activity is makeIntent)
+                (activity as makeIntent).makeIntent(6)
+        }
+
+        return view
     }
 }
