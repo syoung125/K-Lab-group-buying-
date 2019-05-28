741d0937872488884957490dd520b0612d3a31c2
shaelsien630 <shaelsien630@gmail.com>
2019-05-28 23:45:57 +0900

    firebase 연동 및 로그인, 회원가입 구현

--- app/src/main/java/com/example/klapproject/Fragment4.kt
+++ app/src/main/java/com/example/klapproject/Fragment4.kt
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_fragment4.*
 
 class Fragment4 : Fragment() {
 
--- app/src/main/java/com/example/klapproject/SignUpActivity.kt
+++ app/src/main/java/com/example/klapproject/SignUpActivity.kt
@@ -3,19 +3,34 @@ package com.example.klapproject
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import com.google.firebase.database.DatabaseReference
+
 
 class SignUpActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
         init()
     }
+
     fun init() {
         //버튼이벤트
         btnCreateAccount!!.setOnClickListener {
+            //회원가입
+            val userID = ET_su_email.text.toString()
+            val userPW = ET_su_pw.text.toString()
+            val insert = FirebaseDatabase.getInstance().getReference("user").push()
+            insert.child("id").setValue(userID)
+            insert.child("password").setValue(userPW)
+
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
 }
+
+
