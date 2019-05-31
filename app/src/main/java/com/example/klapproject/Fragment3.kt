edb68d8202e88ea4331b73935491c07302e872b4
yeunjin1 <48876807+yeunjin1@users.noreply.github.com>
2019-05-31 22:15:16 +0900

    Merge branch 'master' of C:\Users\yeunj\AndroidStudioProjects\K-Lab-group-buying- with conflicts.

--- app/src/main/java/com/example/klapproject/Fragment3.kt
+++ app/src/main/java/com/example/klapproject/Fragment3.kt
@@ -3,6 +3,7 @@ package com.example.klapproject
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
