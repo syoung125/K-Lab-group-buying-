8f0c0782cc27611c26ee85299b111c2d1a4bd6f8
RessChild <wpqldmlrna@naver.com>
2019-05-31 16:07:25 +0900

    마이페이지 끝

--- app/src/main/java/com/example/klapproject/Fragment4.kt
+++ app/src/main/java/com/example/klapproject/Fragment4.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,12 +16,16 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_record_list.*
 import kotlinx.android.synthetic.main.fragment_fragment4.*
 
 class Fragment4 : Fragment() {
 
     var data = mutableListOf<Int>()
     var textView = mutableListOf<TextView>()
+    var data2 =  mutableListOf<String>()
+    var info =  mutableListOf<RecordData>()
+    lateinit var adapter:RecordAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_fragment4, container, false)
@@ -62,6 +67,7 @@ class Fragment4 : Fragment() {
 
             }
         })
+        load1()
     }
 
     fun setCount(){
@@ -69,4 +75,67 @@ class Fragment4 : Fragment() {
             textView[k].text = data[k].toString() + "명"
     }
 
+
+    fun load1(){
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("user")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val a_list = dataSnapshot.child(MainActivity.u_num.toString()).child("tran_list")
+                var i = 0
+                for (k in a_list.children) {
+                    Log.e("리코드",k.toString())
+                    data2.add(k.child("post_id").value.toString())
+                    i++
+                    if(i == 2)
+                        break
+                }
+                load2(database)
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
+    }
+
+    fun load2(database:FirebaseDatabase){
+        val myRef = database.getReference("post")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                var v=0
+                for (k in dataSnapshot.children) {
+                    Log.e("load2", k.toString())
+
+                    if (k.key.toString() == data2[v]) {
+                        info.add(
+                            RecordData(
+                                k.child("title").value.toString(),
+                                k.child("category").value.toString().toInt()
+                            )
+                        )
+                        v++
+                        if(v == data.size)
+                            break;
+                    }
+                }
+                initAdapter()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
+    }
+
+    fun initAdapter() {
+        //레이아웃을 관리하는 매니저 객체가 필요
+
+        val layoutManager = GridLayoutManager(activity!!.applicationContext,2)
+        // Context 정보, 수평수직 정보, 순서 정보
+        pre_record.layoutManager = layoutManager
+        // recyclerView를 위한 매니저이므로, 붙여줌
+
+        adapter = RecordAdapter(info)
+        pre_record.adapter = adapter //data 정보를 갖는 어댑터를 생성하여 붙여줌
+    }
 }
